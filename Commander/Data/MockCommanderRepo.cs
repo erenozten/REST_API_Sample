022cfc13a7c3e831b09a17e51d0f4a5d9c1fb680
Eren ÖZTEN <priimalshade@gmail.com>
2020-10-21 23:50:14 +0300

    Implemented ICommanderRepo interface to MockCommanderRepo

--- Commander/Data/MockCommanderRepo.cs
+++ Commander/Data/MockCommanderRepo.cs
@@ -3,6 +3,18 @@ using Commander.Models;
 
 namespace Commander.Data{
     public class MockCommanderRepo : ICommanderRepo{
-       
+        public IEnumerable<Command> GetAllCommands(){
+        
+        var commands = new List<Command>(){
+            new Command(){Id=0, HowTo="HowTo1", Line="Line1", Platform="Platform1"},
+            new Command(){Id=1, HowTo="HowTo2", Line="Line2", Platform="Platform2"},
+            new Command(){Id=2, HowTo="HowTo3", Line="Line3", Platform="Platform3"},
+            new Command(){Id=0, HowTo="HowTo4", Line="Line4", Platform="Platform4"}
+        };
+            return commands;
+        }
+        public Command GetCommandById(int id){
+            return new Command(){Id=0, HowTo="", Line="q", Platform="we"};
+        }
     }
 }
