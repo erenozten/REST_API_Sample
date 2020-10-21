40b54a585ea1b3476af504bea4072b5ce549ac02
Eren ÖZTEN <priimalshade@gmail.com>
2020-10-22 00:58:23 +0300

    added comment

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,18 +1,27 @@
 using System.Collections.Generic;
+using Commander.Data;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers{
-    
     // [Route("api/[commands]")] // Bu şekilde de yazılabilir. 
     // Böylece route her zaman sabit kalır. Mantıklı. 
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController: ControllerBase
     {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo(); 
+
         //GET api/commands
         [HttpGet]
         public ActionResult <IEnumerable<Command>> GetAllCommands(){
+        var commandItems = _repository.GetAllCommands();
+
+        // return'e dikkat: 
+        // return Ok yazıp içine commandItems listemizi verdik.
+        // -> Bu metodumuz Ienumerable<Command> return ediyordu.
+        // biz de buna uyarak commandItems'ı return ettik:
+        return Ok(commandItems);
         }
 
         //GET api/commands/{id}
