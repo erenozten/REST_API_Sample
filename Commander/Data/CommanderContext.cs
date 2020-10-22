dcadb22148a5ac3b13563d000d900b1080795463
Eren ÖZTEN <priimalshade@gmail.com>
2020-10-22 04:04:08 +0300

    Create CommanderContext.cs

---
+++ Commander/Data/CommanderContext.cs
@@ -0,0 +1,10 @@
+using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data{
+    public class CommanderContext : DbContext{
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt){
+        }
+        public DbSet<Command> Commands {get;set;}
+    }
+}
